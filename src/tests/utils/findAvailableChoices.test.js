9125489b0294b8f7c530055664ede7c71befa6d3
Scott Nicholes <scott93gps@gmail.com>
2025-03-27 02:10:52 -0700

    Added generateAvailableChoices and adapted tests for this new function

--- src/tests/utils/findAvailableChoices.test.js
+++ src/tests/utils/findAvailableChoices.test.js
@@ -1,25 +1,25 @@
-const countChoices = require('../../utils/findAvailableChoices');
+const generateAvailableChoices = require('../../utils/findAvailableChoices');
 
-test('N: 0, M: 2, to yield S: {2}', () => {
-  expect(countChoices(0, 2, new Set())).toEqual(new Set([2]));
+test('sum: 2, to yield S: {2}', () => {
+  expect(generateAvailableChoices(2)).toEqual(new Set([2]));
 });
 
-test('N: 5, M: 7, to yield S: {5,7,4,8,3,9}', () => {
-  expect(countChoices(5, 7, new Set())).toEqual(new Set([5, 7, 4, 8, 3, 9]));
+test('sum: 12, to yield S: {5,7,4,8,3,9}', () => {
+  expect(generateAvailableChoices(12)).toEqual(new Set([5, 7, 4, 8, 3, 9]));
 });
 
-test('N: 1, M: 2, to yield S: {1,2,3}', () => {
-  expect(countChoices(1, 2, new Set())).toEqual(new Set([1, 2, 3]));
+test('sum: 3, to yield S: {1,2,3}', () => {
+  expect(generateAvailableChoices(3)).toEqual(new Set([1, 2, 3]));
 });
 
-test('N: 4, M: 6, to yield S: {4,6,3,7,2,8,1,9}', () => {
-  expect(countChoices(4, 6, new Set())).toEqual(new Set([4, 6, 3, 7, 2, 8, 1, 9]));
+test('sum: 10, to yield S: {4,6,3,7,2,8,1,9}', () => {
+  expect(generateAvailableChoices(10)).toEqual(new Set([4, 6, 3, 7, 2, 8, 1, 9]));
 });
 
-test('N: 5, M: 6, to yield S: {5,6,4,7,3,8,2,9}', () => {
-  expect(countChoices(5, 6, new Set())).toEqual(new Set([5, 6, 4, 7, 3, 8, 2, 9]));
+test('sum: 11, to yield S: {5,6,4,7,3,8,2,9}', () => {
+  expect(generateAvailableChoices(11)).toEqual(new Set([5, 6, 4, 7, 3, 8, 2, 9]));
 });
 
-test('N: 3, M: 4, to yield S: {3,4,2,5,1,6,7}', () => {
-  expect(countChoices(3, 4, new Set())).toEqual(new Set([3, 4, 2, 5, 1, 6, 7]));
+test('sum: 7, to yield S: {3,4,2,5,1,6,7}', () => {
+  expect(generateAvailableChoices(7)).toEqual(new Set([3, 4, 2, 5, 1, 6, 7]));
 });
--- src/utils/findAvailableChoices.js
+++ src/utils/findAvailableChoices.js
@@ -1,4 +1,18 @@
 /**
+   Generates a set of combinations of numbers that sum to the given sum.
+
+   @param {number} sum A sum that can be rolled with 2 6-sided dice
+ */
+const generateAvailableChoices = (sum) => {
+  const midpoint = Math.floor(sum / 2);
+  const remainder = sum % 2;
+
+  return remainder === 0 ?
+    countChoices(midpoint - 1, midpoint + 1, new Set()) :
+    countChoices(midpoint, midpoint + 1, new Set());
+};
+
+/**
    Creates a set of numbers that represent combinations that sum to the sum of
    N and M.  N and M must not be the same.  If N and M are the same, an empty
    set is returned.
@@ -6,6 +20,7 @@
    @param {number} N The lower component of a sum
    @param {number} M The upper component of a sum
    @param {object} choiceSet A set of numbers that represent possible choices
+   @returns {array} A Set object containing combinations of numbers that sum.
  */
 const countChoices = (N, M, choiceSet) => {
   if (M === 10) {
@@ -20,4 +35,4 @@ const countChoices = (N, M, choiceSet) => {
   }
 }
 
-module.exports = countChoices;
+module.exports = generateAvailableChoices;
