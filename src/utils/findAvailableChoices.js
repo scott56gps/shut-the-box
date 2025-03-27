58d86f67a881aba16154e86dabb8ff0cb9facad9
Scott Nicholes <scott93gps@gmail.com>
2025-03-27 01:08:40 -0700

    Initial Commit.  Added countChoices.

---
+++ src/utils/findAvailableChoices.js
@@ -0,0 +1,21 @@
+/**
+   Creates a set of numbers that represent combinations that sum to the sum of
+   N and M.  N and M must not be the same.  If N and M are the same, an empty
+   set is returned.
+
+   @param {number} N The lower component of a sum
+   @param {number} M The upper component of a sum
+   @param {object} choiceSet A set of numbers that represent possible choices
+ */
+const countChoices = (N, M, choiceSet) => {
+  if (N === 0) {
+    choiceSet.add(M);
+    return choiceSet;
+  } else if (M === 9) {
+    return choiceSet;
+  } else {
+    choiceSet.add(N);
+    choiceSet.add(M);
+    return countChoices(N-1, M+1, choiceSet);
+  }
+}
